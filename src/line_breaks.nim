2d35e2dbff79fd9a26a14f2d27f78ffc7f5ccdaf
taisei0123 <taisei8955@gmail.com>
2022-12-31 20:43:49 +0900

    Adds function to parse an argument of command line

--- src/line_breaks.nim
+++ src/line_breaks.nim
@@ -1,23 +1,42 @@
-import std/[os]
+import std/[os, parseopt]
 
-let filePath = "/Users/idetaisei/html-formater/index.html"
+proc parseFilePath(): string =
+  var p = initOptParser("")
+  while true:
+    p.next()
+    case p.kind
+    of cmdEnd: break
+    of cmdShortOption, cmdLongOption:
+      case p.key:
+        of "f":
+          return p.val
+        else:
+          raise newException(ValueError, "a wrong command flag was specified")
+    of cmdArgument:
+      echo "Argument: ", p.key
 
-if fileExists(filePath):
-  let file = open(filePath, fmRead)
-  var fileContent = file.readAll
+proc formatFileContents(filePath: string): void =
+  if fileExists(filePath):
+    let file = open(filePath, fmRead)
+    var fileContent = file.readAll
 
-  var newContent: string
-  for i in 0..fileContent.len-1:
-    var str = $fileContent[i]
+    var newContent: string
+    for i in 0..fileContent.len-1:
+      var str = $fileContent[i]
 
-    if str == "" or str == "\n" or (str == "." and $fileContent[i-1] == "."):
-      discard
-    elif(str == "." or str == "?"):
-      str = str & "\n"
-      newContent.add str
-    else:
-      newContent.add str
+      if str == "" or str == "\n" or (str == "." and $fileContent[i-1] == "."):
+        discard
+      elif(str == "." or str == "?"):
+        str = str & "\n"
+        newContent.add str
+      else:
+        newContent.add str
 
-  echo newContent
-else:
-  raise newException(ValueError, "The file does not exist")
+    echo newContent
+  else:
+    raise newException(ValueError, "The file does not exist")
+
+let filePath = parseFilePath()
+echo "filePath: ", filePath
+
+formatFileContents(filePath)
